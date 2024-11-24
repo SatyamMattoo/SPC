abedca60c2994b9c6c175eface8de2a24cfdb342
Satyam Mattoo <satyammattoo2003@gmail.com>
2024-11-24 20:18:10 +0000

    flowbite error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite-react/tailwind"
 export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-  ]
+  plugins: [flowbite.plugin()]
 }
 
